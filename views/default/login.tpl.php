9e1938068660aba0c53e81e730cf3ad9ae9903d1
Ekow Abaka Ainooson <jainooson@gmail.com>
2013-07-13 12:24:45 +0000

    Grounding the new login system

--- views/default/login.tpl.php
+++ views/default/login.tpl.php
@@ -1,1 +1,9 @@
-<?php
+<div id="login-box">
+    <h2>Login</h2>
+<?=
+    $helpers->form->open() .
+    $helpers->form->get_text_field('Username', 'username') .
+    $helpers->form->get_password_field('Password', 'password') .    
+    $helpers->form->close('Login');
+?>
+</div>
--- views/default/main.tpl.php
+++ views/default/main.tpl.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-    
+    <?= $helpers->stylesheet->add(load_asset('css/main.css')); ?>
+    <?= $helpers->stylesheet->add(load_asset('css/main.css')); ?>
+    <?= $helpers->stylesheet->add(load_asset('css/forms.css')); ?>
     </head>
     <body>
         <div id="wrapper">
             <div id="head"><h1>Kakalika</h1></div>
-            <div id="body"></div>
+            <div id="body">
+                <?= $contents ?>
+            </div>
             <div id="foot"></div>
         </div>
     </body>

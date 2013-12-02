c654badc8cf3f3e68a41c61b591bf445d369a0f0
Ekow Abaka Ainooson <jainooson@gmail.com>
2013-12-02 20:02:08 +0000

    Separating the issue creation form

--- views/default/issues_create.tpl.php
+++ views/default/issues_create.tpl.php
@@ -1,37 +1,15 @@
 <h4><?= $title ?></h4>
-<?php 
-$helpers->form->setData($data);
-$helpers->form->setErrors($errors);
+<?= 
+t(
+    'issues_form.tpl.php',
+    array(
+        'data' => $data,
+        'errors' => $errors,
+        'assignees' => $assignees,
+        'components' => $components,
+        'milestones' => $milestones
+    )
+) 
 ?>
-<?= $helpers->form->open() ?>
-<div class="row">
-    <div class="column grid_10_7">
-        <div style="padding-right:15px">
-        <?= $helpers->form->get_text_field('Title', 'title')->addCssClass('title') . $helpers->form->get_text_area('Description', 'description')->id('description') ?>
-        </div>
-    </div>
-    <div class="column grid_10_3">
-        <div style="padding-right:1px">
-        <?=
-        $helpers->form->get_selection_list('Assignee', 'assignee')->options($assignees) .
-        $helpers->form->get_selection_list('Kind', 'kind')
-            ->option('Bug', 'BUG')
-            ->option('Enhancement', 'ENHANCEMENT')
-            ->option('Proposal', 'PROPOSAL')
-            ->option('Question', 'QUESTION')
-            ->option('Task', 'TASK'). 
-        $helpers->form->get_selection_list('Priority', 'priority')
-            ->option('Trivial', 'TRIVIAL')
-            ->option('Low', 'LOW')
-            ->option('Medium', 'MEDIUM')       
-            ->option('High', 'HIGH')
-            ->option('Critical', 'CRITICAL')
-            ->option('Blocker', 'BLOCKER').
-        $helpers->form->get_selection_list('Component', 'component')->options($components).
-        $helpers->form->get_selection_list('Milestone', 'milestone')->options($milestones)
-        ?>
-        </div>
-    </div>
-</div>
 
 <?= $helpers->form->close('Create Issue') ?>
---
+++ views/forms/issues_form.tpl.php
@@ -0,0 +1,34 @@
+<?php 
+$helpers->form->setData($data);
+$helpers->form->setErrors($errors);
+?>
+<?= $helpers->form->open() ?>
+<div class="row">
+    <div class="column grid_10_7">
+        <div style="padding-right:15px">
+        <?= $helpers->form->get_text_field('Title', 'title')->addCssClass('title') . $helpers->form->get_text_area('Description', 'description')->id('description') ?>
+        </div>
+    </div>
+    <div class="column grid_10_3">
+        <div style="padding-right:1px">
+        <?=
+        $helpers->form->get_selection_list('Assignee', 'assignee')->options($assignees) .
+        $helpers->form->get_selection_list('Kind', 'kind')
+            ->option('Bug', 'BUG')
+            ->option('Enhancement', 'ENHANCEMENT')
+            ->option('Proposal', 'PROPOSAL')
+            ->option('Question', 'QUESTION')
+            ->option('Task', 'TASK'). 
+        $helpers->form->get_selection_list('Priority', 'priority')
+            ->option('Trivial', 'TRIVIAL')
+            ->option('Low', 'LOW')
+            ->option('Medium', 'MEDIUM')       
+            ->option('High', 'HIGH')
+            ->option('Critical', 'CRITICAL')
+            ->option('Blocker', 'BLOCKER').
+        $helpers->form->get_selection_list('Component', 'component_id')->options($components).
+        $helpers->form->get_selection_list('Milestone', 'milestone_id')->options($milestones)
+        ?>
+        </div>
+    </div>
+</div>

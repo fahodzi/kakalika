adeb0c6c3ae33b08bbcf8594dd8025f559d7e1df
Ekow Abaka Ainooson <jainooson@gmail.com>
2014-03-16 09:48:05 +0000

    Relocating the plugins directory

--- index.php
+++ index.php
@@ -33,6 +33,7 @@ error_reporting(E_ALL  ^ E_NOTICE);
 $ntentan = parse_ini_file('config/ntentan.ini', true);
 require "{$ntentan['home']}/lib/Ntentan.php";
 use ntentan\Ntentan;
+Ntentan::$pluginsHome = "fxplugins/";
 
 // @todo Allow the timezone to be set during installation
 date_default_timezone_set('Africa/Accra');

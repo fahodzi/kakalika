a82a457478fff953eac86644571a92589f9f4301
Ekow Abaka Ainooson <jainooson@gmail.com>
2015-11-15 11:10:33 +0000

    Working on incorporating tests

---
+++ tests/cases/LoginTest.php
@@ -0,0 +1,30 @@
+<?php
+namespace kakalika\tests\cases;
+
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\WebDriverBy;
+
+class LoginTest extends \PHPUnit_Framework_TestCase
+{
+    private $driver;
+    
+    public function setUp()
+    {
+        $host = 'http://localhost:4444/wd/hub';
+        $this->driver = RemoteWebDriver::create($host, DesiredCapabilities::firefox());
+    }
+    
+    public function testLogin()
+    {
+        $this->driver->get('http://kakalika.dev');
+        $this->driver->findElement(WebDriverBy::name('username'))->sendKeys('james');
+        $this->driver->findElement(WebDriverBy::name('password'))->sendKeys('james');
+        $this->driver->findElement(WebDriverBy::cssSelector('.form-submit-area > input'))->click();
+    }
+    
+    public function tearDown()
+    {
+        $this->driver->close();
+    }
+}
